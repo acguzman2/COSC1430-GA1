02eccb7ad94d6e0b32f328ad53fd20848a5ffc20
Austin Vu <vuaustincollege@gmail.com>
2017-09-13 02:05:04 -0500

    Add files via upload

---
+++ 14-ga1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void readGrades(string fileName, int numberOfExams, int numberOfStudents) {
+
+}
+
+void getWeights(double weights[], int numOfExams) {
+
+}
+
+void getAvgsOfExams(double examAvgs[], int numOfExams, int numOfStudents) {
+
+}
+
+void getAvgsOfStudents(double studentAvgs[], double weights[], int numOfExams, int numOfStudents) {
+	
+}
+
+void writeFinalGrades(double examAvgs[], double studentGrades[], int numOfExams, int numOfStudents) {
+
+}
+
+
+int main() {
+	
+	return(0);
+}
