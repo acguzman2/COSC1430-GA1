e4dad2b19f69d90bc8535498a18a9400d253d3f2
Adriana Guzman <acguzman2@uh.edu>
2017-09-14 22:53:01 -0500

    Did writefile

--- 14-ga1.cpp
+++ 14-ga1.cpp
@@ -19,15 +19,15 @@ void readGrades(string fileName, int numberOfExams, int numberOfStudents) {
     input.open(fileName);
 
     // Check for valid file opening
-    if(!input) {
+    if (!input) {
         cerr << "No file";
         return;
     }
 
     // Outer loop for each student
-    for(int i = 0; i < numberOfStudents; ++i) {
+    for (int i = 0; i < numberOfStudents; ++i) {
         // Loop for each exam
-        for(int j = 0; j < numberOfExams; ++j) {
+        for (int j = 0; j < numberOfExams; ++j) {
             // Read each number into each spot in the array
             input >> grades[i][j];
         }
@@ -36,7 +36,7 @@ void readGrades(string fileName, int numberOfExams, int numberOfStudents) {
 }
 
 void getWeights(double weights[], int numOfExams) {
-    for(int i = 0; i < numOfExams; ++i) {
+    for (int i = 0; i < numOfExams; ++i) {
         cout << "Please enter the weight of exam " << i + 1 << ": ";
         cin >> weights[i];
         Sum += weights[i];
@@ -46,9 +46,9 @@ void getWeights(double weights[], int numOfExams) {
 
 // Average of all students on each exam
 void getAvgsOfExams(double examAvgs[], int numOfExams, int numOfStudents) {
-    for(int i = 0; i < numOfExams; ++i) {
+    for (int i = 0; i < numOfExams; ++i) {
         int sum = 0;
-        for(int j = 0; j < numOfStudents; ++j) {
+        for (int j = 0; j < numOfStudents; ++j) {
             sum += grades[j][i];
         }
         examAvgs[i] = (sum / (numOfStudents + 0.0));
@@ -56,22 +56,50 @@ void getAvgsOfExams(double examAvgs[], int numOfExams, int numOfStudents) {
 }
 
 void getAvgsOfStudents(double studentAvgs[], double weights[], int numOfExams, int numOfStudents) {
-
+    for (int i = 0; i < numOfStudents; ++i) {
+        int sum = 0;
+        for (int j = 0; j < numOfExams; ++j) {
+            sum += grades[i][j] * weights[j];
+        }
+        studentAvgs[i] = sum;
+    }
 }
 
 void writeFinalGrades(double examAvgs[], double studentGrades[], int numOfExams, int numOfStudents) {
+    ofstream output;
+    output.open("output.txt");
 
+    if (!output) {
+        cerr << "No file";
+        return;
+    }
+    // Outer loop for each student
+    for (int i = 0; i < numOfStudents; ++i) {
+        // Loop for each exam
+        for (int j = 0; j < numOfExams; ++j) {
+            // Read each number into each spot in the array
+            output << grades[i][j] << " ";
+        }
+        output << studentGrades[i] << endl;
+    }
+
+    for(int i = 0; i < numOfExams; ++i) {
+        output << examAvgs[i] << " ";
+    }
+    output.close();
 }
 
 
 int main() {
     readGrades(FileName, NumberOfExams, NumberOfStudents);
     getWeights(Weights, NumberOfExams);
-    if(Sum != 1) {
+    if (Sum != 1) {
         cerr << "Invalid weight distribution" << endl;
         return 1;
     }
-
+    getAvgsOfExams(examAvgs, NumberOfExams, NumberOfStudents);
+    getAvgsOfStudents(studentAvgs, Weights, NumberOfExams, NumberOfStudents);
+    writeFinalGrades(examAvgs, studentAvgs, NumberOfExams, NumberOfStudents);
 
     return (0);
 }
